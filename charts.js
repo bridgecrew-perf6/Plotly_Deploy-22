d5824564524f8b8579a45eea8865a11181205c6e
hmart424 <95835840+hmart424@users.noreply.github.com>
2022-03-26 23:16:47 -0500

    Update charts.js

--- charts.js
+++ charts.js
@@ -6,31 +6,21 @@ function buildCharts(sample) {
 
     // Create a variable that holds the samples array. 
     var samples = data.samples;
+    console.log(samplesArray);    
     // Create a variable that filters the samples for the object with the desired sample number.
-    var resultSample = samples.filter(sampleObj => sampleObj.id == sample);
-    console.log(resultSample);
-    
+    var selectedIdSamples = samplesArray.filter(data => data.id == sample);
+    console.log(selectedIdSamples);    
     // Create a variable that holds the first sample in the array.
-    var resultForSample = resultSample[0];
-    console.log(resultForSample);
-
-    // 2. Create a variable that holds the first sample in the metadata array.
-    var metadata = data.metadata[0];
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    console.log(resultArray);
-
+    var selectedIdSamples = samplesArray.filter(data => data.id == sample)
+    var firstSample = selectedIdSamples[0];
+    console.log(firstSample);
     // Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otuIds = resultForSample.otu_ids
-    var otuIdsSliced = otuIds.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
-    console.log(otuIdsSliced)
-    
-    var otuLables = resultForSample.otu_labels;
-    var otuLablesSliced = otuLables.slice(0,10).reverse();
-    console.log(otuLablesSliced);
-
-    var sampleValues = resultForSample.sample_values;
-    var sampleValuesSliced = sampleValues.slice(0,10).reverse();
-    console.log(sampleValuesSliced);
+    var otuIds = firstSample.otu_ids;
+    var otuLabels = firstSample.otu_labels;
+    var sampleValues = firstSample.sample_values;
+    console.log(otuIds);
+    console.log(otuLabels);
+    console.log(sampleValues);   
     // 3. Create a variable that holds the washing frequency.
     var washFreq = +metadata_SelId[0].wfreq;
     // Create the yticks for the bar chart.
@@ -44,35 +34,46 @@ function buildCharts(sample) {
    
     
     // 4. Create the trace for the gauge chart.
-    var gaugeData = [{
-      title: {text: "Scrubs per Week", font: {size: 18}},
-      type: "indicator",
-      mode: "gauge+number",
-      value: wFreq,
-      tickmode: 'linear',
-      gauge: {
-        axis: { range: [null, 10], dtick: 2, tick0: 0 },
-        bar: { color: "firebrick" },
-        bgcolor: "white",
-        borderwidth: 2,
-        bordercolor: "gray",
-        steps: [
-          { range: [0, 2], color: "floralwhite"},
-          { range: [2, 4], color: "lavender"},
-          { range: [4, 6], color: "thistle"},
-          { range: [6, 8], color: "mediumslateblue" },
-          { range: [8, 10], color: "royalblue" },
-        ]},
-    };
-    
+    var gaugeData = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: washFreq,
+        title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs per week"},
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: {
+            range: [null, 10],
+            tickmode: "array",
+            tickvals: [0,2,4,6,8,10],
+            ticktext: [0,2,4,6,8,10]
+          },
+          bar: {color: "black"},
+          steps: [
+            { range: [0, 2], color: "red" },
+            { range: [2, 4], color: "orange" },
+            { range: [4, 6], color: "yellow" },
+            { range: [6, 8], color: "lime" },
+            { range: [8, 10], color: "green" }]
+        }
+      }
+    ];    
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-      title: "Belly Button Washing Frequency",
-      titlefont: {"size": 25}
+      autosize: true,
+      annotations: [{
+        xref: 'paper',
+        yref: 'paper',
+        x: 0.5,
+        xanchor: 'center',
+        y: 0,
+        yanchor: 'center',
+        text: "The gauge displays your belly button weekly washing frequency",
+        showarrow: false
+      }]
     };
-
     // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot("gauge" , gaugeData, gaugeLayout);
+    Plotly.newPlot("gauge" , gaugeData, gaugeLayout,{responsive: true});
   });
 }
 
@@ -95,7 +96,7 @@ function buildCharts(sample) {
       marker: {
         size: sampleValues,
         color: otuIds,
-        colorscale: 'RdBu',
+        colorscale: 'Earth',
       }
    
     }];
